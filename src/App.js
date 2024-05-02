b0a53984d0028576c1d24c93627312414ba7f3da
Diogo Souza <115508662+DiogoSouza08@users.noreply.github.com>
2024-05-01 23:28:31 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+// App.js
+
+import React from 'react';
 import './App.css';
 import CardsMandF from './components.js/CardsMadnF';
 import ClothingOptions from './components.js/ClothingOptions';
@@ -11,72 +14,75 @@ import EmailInput from './components.js/emailInput';
 import Rodape from './components.js/footer';
 
 
-function App() {
 
+const App = () => {
   return (
-    <div className="App bg-zinc-700 ">
-      <div className='flex items-center justify-center py-2.5 sm:hidden border-gray-400 border-b'>
-        <img src="jordan-logo.webp" alt="" className='w-12' />
-      </div>
-      <header className="App-header z-40">
-        <main className='flex-col  bg-white sm:pb-0 pb-0 flex  '>
-          <Header />
-          <div className=' px-3.5 lg:px-24  justify-between flex-col-reverse sm:flex-row flex  items-center '>
-            <div className='pb-20 sm:pb-0 h-auto  items-start space-y-6 text-center sm:text-start   flex-col justify-center  flex'>
-              <span className='title font-bold text-red-700 text-8xl lg:text-9xl'>THE LAST<br></br> DANCE</span>
-              <p className='text-zinc-700 text-xl'>Descubra a história por trás da <br></br>coleção Air Jordan</p>
+    <div className='bg-zinc-700 w-full flex items-center justify-center'>
+      <div className="App max-w-screen-2xl ">
+        <div className='flex items-center justify-center py-2.5 sm:hidden border-gray-400 border-b'>
+          <img src="jordan-logo.webp" alt="" className='w-12' />
+        </div>
+        <header className="App-header z-40">
+          <main className='flex-col bg-white sm:pb-0 pb-0 flex'>
+            <Header />
+            <div className='px-3.5 lg:px-24 justify-between flex-col-reverse sm:flex-row flex items-center'>
+              <div className='pb-20 sm:pb-0 h-auto items-start space-y-6 text-center sm:text-start flex-col justify-center flex'>
+                <span className='title font-bold text-red-700 text-8xl lg:text-9xl'>THE LAST<br /> DANCE</span>
+                <p className='text-zinc-700 text-xl'>Descubra a história por trás da <br />coleção Air Jordan</p>
+              </div>
+              <img src="https://cdn.afew-store.com/assets/38/388297/1200/air-jordan-1-retro-high-og-sp-university-red-black-summit-white-dv1748-601-footwear%20%3E%20sneaker-packshots-120.jpg" alt="" className='w-full md:w-45 sm:w-96 ' />
             </div>
-            <img src="https://cdn.afew-store.com/assets/38/388297/1200/air-jordan-1-retro-high-og-sp-university-red-black-summit-white-dv1748-601-footwear%20%3E%20sneaker-packshots-120.jpg" alt="" className=' w-full md:w-45 sm:w-96 ' />
-          </div>
-        
-        </main>
-      </header>
-      <section>
+          </main>
+        </header>
+
+  
+
         <CardsMandF />
-      </section>
-      <section>
-        <CustomSection
-          backgroundImage='https://images.unsplash.com/photo-1640000013087-842012285d30?q=80&w=1567&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-          title="O que você está procurando?"
-          subtitle="Temos qualquer tipo de Air Jordan que levanta sua alma!"
-        >
-          <BuyBtn />
-        </CustomSection>
-      </section>
-      <section className='lg:px-24 gap-10 flex flex-col px-5 text-gray-400 justify-center text-center items-center py-24'>
-        <div className='gap-2 flex flex-col'>
-          <span className='title uppercase sm:text-8xl text-6xl  font-extrabold '>Roupas</span>
-          <p>Descubra a essência jordan</p>
-        </div>
-        <ClothingOptions />
-      </section>
-      <section>
-        <CustomSection
-          backgroundImage='https://images.unsplash.com/photo-1565261189855-95dac103929d?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-          title="Você nas alturas"
-          subtitle="O melhor da Jordan, estilo moderno, utilidade de rua!"
-        >
-          <BuyBtn />
-        </CustomSection>
-      </section>
-      <section className='lg:px-24 gap-10 flex flex-col px-5 text-gray-400 justify-center text-center items-center py-24'>
-        <span className='title uppercase sm:text-8xl text-6xl font-black '>Coleção Air Jordan</span>
-        <ProductCard />
-        <SeeAllBtn />
-      </section >
-      <section className='lg:px-24 gap-10 flex flex-col px-5 text-gray-400 justify-center text-center items-center py-24'>
-        <span className='title uppercase text-3xl sm:text-4xl font-black '>Inscreva-se para receber notícias e promoções</span>
-        <EmailInput />
-      </section>
-      <footer>
-        <Rodape />
-        <div className='border-gray-400 py-3 text-gray-400 justify-center items-center text-sm border-b flex text-center'>
-          <div className='flex items-center justify-center flex-col gap-4'>
-            <img src="./jordan-logo.webp" alt="" className='w-10 sm:hidden' />
-         <span>Todos os direitos reservados por Diogo Souza©</span>
-         </div> 
-        </div>
-      </footer>
+
+        <section>
+          <CustomSection
+            backgroundImage='https://images.unsplash.com/photo-1640000013087-842012285d30?q=80&w=1567&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            title="O que você está procurando?"
+            subtitle="Temos qualquer tipo de Air Jordan que levanta sua alma!"
+          >
+            <BuyBtn />
+          </CustomSection>
+        </section>
+        <section className='lg:px-24 gap-10 flex flex-col px-5 text-gray-400 justify-center text-center items-center py-24'>
+          <div className='gap-2 flex flex-col'>
+            <span className='title uppercase sm:text-8xl text-6xl font-extrabold '>Roupas</span>
+            <p>Descubra a essência jordan</p>
+          </div>
+          <ClothingOptions />
+        </section>
+        <section>
+          <CustomSection
+            backgroundImage='https://images.unsplash.com/photo-1565261189855-95dac103929d?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            title="Você nas alturas"
+            subtitle="O melhor da Jordan, estilo moderno, utilidade de rua!"
+          >
+            <BuyBtn />
+          </CustomSection>
+        </section>
+        <section className='lg:px-24 gap-10 flex flex-col px-5 text-gray-400 justify-center text-center items-center py-24'>
+          <span className='title uppercase sm:text-8xl text-6xl font-black '>Coleção Air Jordan</span>
+          <ProductCard />
+          <SeeAllBtn />
+        </section >
+        <section className='lg:px-24 gap-10 flex flex-col px-5 text-gray-400 justify-center text-center items-center py-24'>
+          <span className='title uppercase text-3xl sm:text-4xl font-black '>Inscreva-se para receber notícias e promoções</span>
+          <EmailInput />
+        </section>
+        <footer>
+          <Rodape />
+          <div className='border-gray-400 py-3 text-gray-400 justify-center items-center text-sm border-b flex text-center'>
+            <div className='flex items-center justify-center flex-col gap-4'>
+              <img src="./jordan-logo.webp" alt="" className='w-10 sm:hidden' />
+              <span>Todos os direitos reservados por Diogo Souza©</span>
+            </div>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/components.js/menuOptions.js
+++ src/components.js/menuOptions.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import ManIcon from '@mui/icons-material/Man';
-import { Woman } from '@mui/icons-material';
+
 
 const MenuOptions = ({ layout, isMobileHeader }) => {
   const menuClass = layout === 'horizontal' ? 'flex' : 'flex-col';
@@ -10,23 +9,18 @@ const MenuOptions = ({ layout, isMobileHeader }) => {
     <div>
       <ul className={`gap-10 flex text-gray-300 ${textSize} lg:flex ${menuClass}`}>
         <li>
-          <span className=" lg:hidden"></span>
           MASCULINO
         </li>
         <li>
-          <span className="lg:hidden"></span>
           FEMININO
         </li>
         <li>
-          <span className="lg:hidden"></span>
           KIDS
         </li>
         <li>
-          <span className="lg:hidden"></span>
           CALÇADOS
         </li>
         <li>
-          <span className="lg:hidden"></span>
           ROUPAS
         </li>
       </ul>
